38e295a0f7f3d742417b26aed2191d45ab464e67
erin.major <erinmajor254@gmail.com>
2024-10-17 00:50:50 +0100

    Add dynamic localStorage, format the page

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -2,11 +2,46 @@ import displayFooter from "./footer.mjs";
 
 const menu = document.querySelector('#menu');
 const navElement = document.querySelector('#animation');
-
+const visitMessage = document.querySelector('#visit-message');
 
 menu.addEventListener('click', () => {
     menu.classList.toggle("open");
     navElement.classList.toggle("open");
 });
+
+function updateVisit() {
+    const lastVisit = localStorage.getItem('lastVisit');
+    const millisecondsInADay = 24 * 60 * 60 * 1000;
+    let today = Date.now();   
+     
+    if (lastVisit == null) {        
+        visitMessage.innerHTML = `Welcome! Let us know if you have any questions.`;
+    }
+
+    else {
+        let timeSinceLastVisit = today - parseInt(lastVisit);
+
+        if (timeSinceLastVisit < millisecondsInADay) {
+            
+            visitMessage.innerHTML = `Back so soon! Awesome!`;
+        }
+
+        else {
+            let diffDays = Math.round(timeSinceLastVisit / millisecondsInADay);
+            
+            if (diffDays === 1) {
+                visitMessage.innerHTML = `You last visited ${diffDays} day ago.`;
+            }
+
+            else {
+                visitMessage.innerHTML = `You last visited ${diffDays} days ago.`;
+            }
+        }
+    }
+
+    localStorage.setItem('lastVisit', today);
+};
+
+updateVisit();
 
 displayFooter();
