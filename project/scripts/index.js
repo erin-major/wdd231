0924a719a1b137567169567e6ba6e5717b70ddd2
erin.major <erinmajor254@gmail.com>
2024-10-23 00:15:12 +0100

    Add functions and APIs to grab ip address and location

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -3,9 +3,39 @@ import displayFooter from "./footer.mjs";
 const menu = document.querySelector('#menu');
 const navElement = document.querySelector('#animation');
 
+let ipUrl = "https://api.aruljohn.com/ip/json";
+let ipAddress = null;
+let locationUrl = null;
+let location = null;
+
 menu.addEventListener('click', () => {
     menu.classList.toggle("open");
     navElement.classList.toggle("open");
 });
+
+async function getIpAddress() {
+    const response = await fetch(ipUrl);
+    const ipAddress = await response.json();
+    return ipAddress.ip;
+};
+
+async function getLocation() {
+    const response = await fetch(locationUrl);
+    const location = await response.json();
+    return location;
+};
+
+(async () => {
+    ipAddress = await getIpAddress();    
+    locationUrl = `https://ipapi.co/${ipAddress}/json/`;
+    
+    (async () => {
+        location = await getLocation();
+        console.log(location);
+    })();
+})();
+
+
+
 
 displayFooter();
