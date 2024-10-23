89ef7a51ea30ab34169c516be7495077b2644a06
erin.major <erinmajor254@gmail.com>
2024-10-24 00:18:28 +0100

    Get aurora percentage and add images

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -2,40 +2,91 @@ import displayFooter from "./footer.mjs";
 
 const menu = document.querySelector('#menu');
 const navElement = document.querySelector('#animation');
+const viewingChance = document.querySelector('#viewing-chance');
 
-let ipUrl = "https://api.aruljohn.com/ip/json";
+const ipUrl = "https://api.aruljohn.com/ip/json";
 let ipAddress = null;
 let locationUrl = null;
-let location = null;
+let lat = null;
+let long = null;
+let auroraUrl = null;
+let auroraPercent = null;
 
 menu.addEventListener('click', () => {
     menu.classList.toggle("open");
     navElement.classList.toggle("open");
 });
 
-async function getIpAddress() {
-    const response = await fetch(ipUrl);
-    const ipAddress = await response.json();
-    return ipAddress.ip;
+async function getIpAddress(url) {    
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            let data = await response.json();           
+            ipAddress = data.ip;
+            locationUrl = `https://get.geojs.io/v1/ip/geo/${ipAddress}.json`;
+        }
+        else {
+            throw Error(await response.text());
+        }
+    }
+    catch(e) {
+        console.log(e);
+    }
 };
 
-async function getLocation() {
-    const response = await fetch(locationUrl);
-    const location = await response.json();
-    return location;
+async function getLocation(url) {    
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            let data = await response.json();           
+            lat = data.latitude;
+            long = data.longitude;
+            auroraUrl = `https://api.auroras.live/v1/?type=all&lat=${lat}&long=${long}&forecast=false&threeday=false`;
+        }
+        else {
+            throw Error(await response.text());
+        }
+    }
+    catch(e) {
+        console.log(e);
+    }
+};
+
+async function getAuroraInformation(url) {    
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            let data = await response.json();
+            auroraPercent = data.probability.value;
+            displayViewingChance();           
+        }
+        else {
+            throw Error(await response.text());
+        }
+    }
+    catch(e) {
+        console.log(e);
+    }
+};
+
+function displayViewingChance() {
+
+    if (auroraPercent < 30)
+    {
+        viewingChance.textContent = `Sorry, you have a ${auroraPercent}% chance of seeing aurora borealis right now.`;
+    }
+    else if (auroraPercent < 60) {
+        viewingChance.textContent = `Fingers crossed! You have a ${auroraPercent}% chance of seeing aurora borealis right now.`;
+    }
+    else {
+        auroraPercent.textContent = `Get outside! You have a ${auroraPercent}% chance of seeing aurora borealis right now!`;
+    }
 };
 
 (async () => {
-    ipAddress = await getIpAddress();    
-    locationUrl = `https://ipapi.co/${ipAddress}/json/`;
-    
-    (async () => {
-        location = await getLocation();
-        console.log(location);
-    })();
+    await getIpAddress(ipUrl);
+    await getLocation(locationUrl);
+    await getAuroraInformation(auroraUrl);
 })();
-
-
-
 
 displayFooter();
